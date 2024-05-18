ba5c5284b61c34391fc72ee6c02262eb9cfbc8e7
Nikita Tchayka <nikitatchayka@gmail.com>
2024-05-18 21:26:01 +0100

    Format

--- src/Playground.hs
+++ src/Playground.hs
@@ -1,16 +1,17 @@
 module Playground (init) where
 
-import Core
-import Text qualified
 import Array qualified
 import Console qualified
+import Core
+import Text qualified
 
 init :: IO Unit
 init = do
-  let stuff = "NeoHaskell is cool"
-              |> Text.words
-              |> Array.map Text.length
-              |> Array.map (\x -> x * x)
-              |> Text.tshow
+  let stuff =
+        "NeoHaskell is cool"
+          |> Text.words
+          |> Array.map Text.length
+          |> Array.map (\x -> x * x)
+          |> Text.tshow
 
   Console.print stuff
